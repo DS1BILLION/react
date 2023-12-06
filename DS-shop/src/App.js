c4787d286298d321f9f23b09cd60f50cf780555d
DS1BILLION <divyanshusharma469@gmail.com>
2023-12-06 10:55:49 +0530

    chamgd a lot XD

--- DS-shop/src/App.js
+++ DS-shop/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div className="header-logo"></div>
+
+      <div className="header-search"></div>
+      
+      <div className="header-navigation">
+
+      <div className="nav-item">
+        <a href="">Home</a>
+      
+      </div>
+
+      <div className="nav-item">
+        <a href="">shop</a>
+      
+      </div>
+      
+      <div className="nav-item">
+        <a href="">cart</a>
+      
+      </div>
+
+      </div>
+
       </header>
     </div>
   );
